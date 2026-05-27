eeaa93a29b658c2cd7113fe853a22f34af623af4
dayannamarin1626 <marinmishell164@gmail.com>
2026-05-26 21:09:26 -0500

    mensagge

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8330c39a47e7052abac87ad32f1ce02c4f3fb599',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -443,9 +443,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8330c39a47e7052abac87ad32f1ce02c4f3fb599',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
